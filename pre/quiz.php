c7f422284736dffcaff9d76350f9fb1fa415feb2
kay-ws <a3.works@gmail.com>
2019-07-28 12:35:14 +0000

    Sun Jul 28 12:35:14 UTC 2019 quiz.php refactoring

--- pre/quiz.php
+++ pre/quiz.php
@@ -1,3 +1,53 @@
+<?php
+define('CHECKED', 'checked');
+define('ANSWER1_CORRECT', '24');
+define('MESSAGE_CORRECT', '<div class="correct">正解です。</div>');
+define('MESSAGE_INCORRECT', '<div class="incorrect">不正解です。</div>');
+
+$answer1 = '';
+$answer1_comment ='';
+$answer2 = '';
+$answer2_checked_1 = '';
+$answer2_checked_2 = '';
+$answer2_checked_3 = '';
+$answer2_comment = '';
+$answer3 = '';
+$answer3_checked_1 = '';
+$answer3_checked_2 = '';
+$answer3_checked_3 = '';
+$answer3_comment = '';
+
+$is_executed = (isset($_POST['send'])) ? true : false;
+
+//回答１
+$answer1 = (isset($_POST['text1']) && is_string($_POST['text1']))? $_POST['text1'] : '';
+if ($is_executed) {
+  $answer1_comment = ($answer1 === ANSWER1_CORRECT) ? MESSAGE_CORRECT : MESSAGE_INCORRECT;
+}
+
+//回答２
+$answer2 = (isset($_POST['check1']) && is_array($_POST['check1'])) ? $_POST['check1'] : '';
+if ($answer2 !== '') {
+  foreach ($answer2 as $value) {
+    ${"answer2_checked_" . $value} = CHECKED;
+  }
+}
+if ($is_executed) {
+  $answer2_comment = ($answer2_checked_1 === CHECKED 
+                    && $answer2_checked_2 === CHECKED
+                    && $answer2_checked_3 === '') ? MESSAGE_CORRECT : MESSAGE_INCORRECT;
+}
+
+//回答３
+$answer3 = (isset($_POST['radio1']) && is_string($_POST['radio1'])) ? $_POST['radio1'] : '';
+if ($answer3 !== '') ${"answer3_checked_" . $answer3} = CHECKED;
+if ($is_executed) {
+//  $answer3_comment = ($answer3_checked_1 === ''
+//                    && $answer3_checked_2 === CHECKED
+//                    && $answer3_checked_3 === '') ? MESSAGE_CORRECT : MESSAGE_INCORRECT;
+  $answer3_comment = ($answer3_checked_2 === CHECKED) ? MESSAGE_CORRECT : MESSAGE_INCORRECT;
+}
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -15,107 +65,34 @@
     </style>
   </head>
   <body>
-  <?php
-    if (isset($_POST['text1'])) {
-      $init_flag = false;
-    } else {
-      $init_flag = true;
-    }
-//    var_dump($init_flag);
-    if (isset($_POST['text1']) && is_string($_POST['text1'])) {
-      $a1 = $_POST['text1'];
-    } else {
-      $a1 = '';
-    }
-    
-    $a2_1 = '';
-    $a2_2 = '';
-    $a2_3 = '';
-    if (isset($_POST['check1']) && is_array($_POST['check1'])) {
-      $a2 = $_POST['check1'];
-      foreach ($a2 as $value) {
-        ${"a2_" . $value} = 'checked';
-      }
-    } else {
-      $a2 = '';
-    }
-    
-    $a3_1 = '';
-    $a3_2 = '';
-    $a3_3 = '';
-    if (isset($_POST['radio1']) && is_string($_POST['radio1'])) {
-      $a3 = $_POST['radio1'];
-      ${"a3_" . $a3} = 'checked';
-    } else {
-      $a3 = '';
-    }
-    
-//    var_dump($a1, $a2, $a3);
-   ?>
    <form method="POST" action"./quiz.php">
       <p>
         [Q1] ４の階乗（4!）の値は？：
-        <input type="text" name="text1" value=<?= $a1 ?>></input>
+        <input type="text" name="text1" value=<?= $answer1 ?>></input>
       </p>
       <p>
-      <?php
-        if (!$init_flag) { //初期状態では表示しない
-          if ($a1 === "24") {
-      ?>
-            <div class="correct">正解です。</div>
-      <?php  
-          } else {
-      ?>
-            <div class="incorrect">不正解です。</div>
-      <?php  
-          }
-        }
-      ?>
+        <?= $answer1_comment ?>
       </p>
         
       <p>
         [Q2]日本にあるのは？：
-        <input type="checkbox" name="check1[]" value="1" <?= $a2_1 ?>>京都タワー</input>
-        <input type="checkbox" name="check1[]" value="2" <?= $a2_2 ?>>東京タワー</input>
-        <input type="checkbox" name="check1[]" value="3" <?= $a2_3 ?>>上海タワー</input>
+        <input type="checkbox" name="check1[]" value="1" <?= $answer2_checked_1 ?>>京都タワー</input>
+        <input type="checkbox" name="check1[]" value="2" <?= $answer2_checked_2 ?>>東京タワー</input>
+        <input type="checkbox" name="check1[]" value="3" <?= $answer2_checked_3 ?>>上海タワー</input>
       </p>
       <p>
-      <?php
-        if(!$init_flag) {
-          if ($a2_1 === 'checked' && $a2_2 === 'checked' && $a2_3 === '') {
-      ?>
-            <div class="correct">正解です。</div>
-      <?php
-          } else {
-      ?>
-            <div class="incorrect">不正解です。</div>
-      <?php
-          }
-        }
-      ?>
+        <?= $answer2_comment ?>
       </p>
       <p>
         [Q3]一番小さいのは？：
-        <input type="radio" name="radio1" value=1 <?= $a3_1 ?>>１ｍ</input>
-        <input type="radio" name="radio1" value=2 <?= $a3_2 ?>>一寸</input>
-        <input type="radio" name="radio1" value=3 <?= $a3_3 ?>>一里</input>
+        <input type="radio" name="radio1" value=1 <?= $answer3_checked_1 ?>>１ｍ</input>
+        <input type="radio" name="radio1" value=2 <?= $answer3_checked_2 ?>>一寸</input>
+        <input type="radio" name="radio1" value=3 <?= $answer3_checked_3 ?>>一里</input>
       </p>
       <p>
-      <?php
-        if (!$init_flag) {
-          if ($a3_2 === 'checked') {
-      ?>
-            <div class="correct">正解です。</div>
-      <?php
-          } else {
-      ?>
-            <div class="incorrect">不正解です。</div>
-      <?php
-          }
-        }
-      ?>
-      
-      <input type="submit" name="button1" value="回答する"></input>
+        <?= $answer3_comment ?>
+      </p>
+      <input type="submit" name="send" value="回答する"></input>
    </form>
  </body>
   </head>
