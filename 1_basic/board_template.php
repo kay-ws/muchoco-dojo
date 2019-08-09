6afd2ce0ce831e5d41b9810f01202ac80634d069
kay-ws <a3.works@gmail.com>
2019-08-09 13:36:23 +0000

    Fri Aug  9 13:36:23 UTC 2019 board_template.php modifying

--- 1_basic/board_template.php
+++ 1_basic/board_template.php
@@ -2,18 +2,28 @@
 <html lang="ja">
  <head>
    <meta charset="utf-8">
-   <title>掲示板（ログイン・新規登録）</title>
+   <title>掲示板</title>
  </head>
  <body>
-   <div class="infoMessage"><?= $info ?></div>
-
-   <h1>ログイン</h1>
-   <form action="login.php" method="post">
-     <label for="email">email</label>
-     <input type="email" name="email">
-     <label for="password">password</label>
-     <input type="password" name="password">
-     <button type="submit" name ="login">ログイン</button>
+    <div class="infoMessage"><?= $info ?></div>
+    <form action="post.php" method="post">
+      <button type="submit" name="post">新規投稿</button>
+    </form>
+    <form action="logout.php" method="post">
+      <button type="submit" name="logout">ログアウト</button>
+    </form>
+    
+    <div class="post">
+      
+    </div>
+    
+    <h1>ログイン</h1>
+    <form action="login.php" method="post">
+      <label for="email">email</label>
+      <input type="email" name="email">
+      <label for="password">password</label>
+      <input type="password" name="password">
+      <button type="submit" name ="login">ログイン</button>
    </form>
    <h1>新規登録</h1>
    <form action="signUp.php" method="post">
