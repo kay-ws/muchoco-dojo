cadea4fc9b029b5e6e53a570082efe0745f0f42c
kay-ws <a3.works@gmail.com>
2019-08-09 08:48:17 +0000

    Fri Aug  9 08:48:17 UTC 2019 1_basic/*.php 6 add

---
+++ 1_basic/config.php
@@ -0,0 +1,10 @@
+<?php
+//https://qiita.com/qwertyuiopngsdfg/items/597da67387723a5aedad
+
+//ini_set('display_errors', 1);
+
+define('DSN', 'mysql:host=localhost;dbname=test_login');
+define('DB_USER', 'hogeUser');
+define('DB_PASS', 'hogehoge');
+
+?>
