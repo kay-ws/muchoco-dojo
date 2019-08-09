b4c4be3604b06cafc0440fd99b8b5cd123a3a74e
kay-ws <a3.works@gmail.com>
2019-08-09 20:18:25 +0000

    Fri Aug  9 20:18:25 UTC 2019 work break snap

--- 1_basic/board_template.php
+++ 1_basic/board_template.php
@@ -1,3 +1,7 @@
+<?php
+  require_once('config.php');
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
  <head>
@@ -6,15 +10,24 @@
  </head>
  <body>
     <div class="infoMessage"><?= $info ?></div>
+
     <form action="post.php" method="post">
       <button type="submit" name="post">新規投稿</button>
     </form>
     <form action="logout.php" method="post">
       <button type="submit" name="logout">ログアウト</button>
     </form>
+<?php
+    try {
+      $pdo = new PDO(DSN, DB_USER, DB_PASS);
+      $stmt = $pdo->prepare('select * from posts where email = ?');
+      $stmt->execute([$_POST['email']]);
+      
+      
+    }
     
     <div class="post">
-      
+    <h2>投稿：<?= ?></h2>  
     </div>
     
     <h1>ログイン</h1>
--- 1_basic/config.php
+++ 1_basic/config.php
@@ -1,10 +1,8 @@
 <?php
 //https://qiita.com/qwertyuiopngsdfg/items/597da67387723a5aedad
 
-//ini_set('display_errors', 1);
-
-define('DSN', 'mysql:host=localhost;dbname=test_login');
-define('DB_USER', 'hogeUser');
-define('DB_PASS', 'hogehoge');
+define('DSN', 'mysql:host=localhost;dbname=board');
+define('DB_USER', 'boardMentainer');
+define('DB_PASS', 'board');
 
 ?>
