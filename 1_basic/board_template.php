36c1a43a3d3000b02210caa202d299a818a52984
kay-ws <a3.works@gmail.com>
2019-08-10 06:23:46 +0000

    Sat Aug 10 06:23:46 UTC 2019 post.php add

--- 1_basic/board_template.php
+++ 1_basic/board_template.php
@@ -33,6 +33,8 @@
           <h2>投稿：<?= $row['email'] ?></h2>
           <img src="<?= $row['imagePath'] ?>"
           <p>message:<?= $row['message'] ?></p>
+          <button type="submit" name="commentPost" method="post">コメントする</button>
+          <button type="submit" name="deletePost" method="post">削除</button>
         </form>
 <?php
         try {
@@ -43,10 +45,12 @@
         }
         foreach ($stmt2 as $row2) {
 ?>
-          <form action="comment.php>
+          <form action="comment.php">
             <h4>コメント：<?= $row['email'] ?></h4>
             <img src="<?= $row['imagePath'] ?>"
             <span>message:<?= $row['message'] ?></span>
+            <button type="submit" name="commentPost" method="post">コメントする</button>
+            <button type="submit" name="deletePost" method="post">削除</button>
           </form>
 <?php
         }
